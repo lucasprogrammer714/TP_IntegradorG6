477287cb623f10886f7d188627d828441a8a3ace
FidelBurgos <fidelburgos97@gmail.com>
2021-06-29 18:16:34 -0300

    Funcion Insertar Cliente y servlet cliente corregido

--- src/DaoImpl/ClienteDaoImpl.java
+++ src/DaoImpl/ClienteDaoImpl.java
@@ -1,5 +1,6 @@
 package DaoImpl;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -18,12 +19,12 @@ public class ClienteDaoImpl implements ClienteDao {
 	/// iria en servletCliente --setFechaNacimiento(Date.valueOf(request.getParameter("dateFechaNacimiento")));
 	
 	
-	private static final String insert = "INSERT INTO clientes(DNI, cuil, nombre, apellido) VALUES(?, ?, ?,?)";
+	private static final String insert = "INSERT INTO clientes(DNI, cuil, nombre, apellido, sexo, nacionalidad, nacimiento, direccion, localidad, provincia, email, telefono, usuario, contrasena, admin, estado, telefono_fijo) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 	
 	public boolean insertar(Cliente cliente) {
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 		}catch(ClassNotFoundException e){
 			e.printStackTrace();
@@ -35,25 +36,39 @@ public class ClienteDaoImpl implements ClienteDao {
 		try{
 			
 			
-			
+			//System.out.println("DNI: " + cliente.getDni().toString());
+			//System.out.println("CUIL: " + cliente.getCuil().toString());
+			//System.out.println("NOMBRE: " + cliente.getNombre());
+			//System.out.println("APELLIDO: " + cliente.getApellido().toString());
+			//System.out.println("SEXO: " + cliente.getSexo().toString());
+			//System.out.println("NACIONALIDAD: " + cliente.getNacionalidad().toString());
+			//System.out.println("NACIMIENTO" + cliente.getNacimiento());
+			//System.out.println("DIRECCION: " + cliente.getDireccion().toString());
+			//System.out.println("LOCALIDAD: " + cliente.getLocalidad().toString());
+			//System.out.println("PROVINCIA: " + cliente.getProvincia().toString());
+			//System.out.println("EMAIL: " + cliente.getEmail().toString());
+			//System.out.println("TELEFONO: " + cliente.getTelefono().toString());
+			//System.out.println("USUARIO: " + cliente.getUsuario().toString());
+			//System.out.println(cliente.toString());
+			//System.out.println(cliente.toString());
 			statement = conexion.prepareStatement(insert);
 			statement.setString(1, cliente.getDni());
 			statement.setString(2, cliente.getCuil());
 			statement.setString(3, cliente.getNombre());
 			statement.setString(4, cliente.getApellido());
-			/*statement.setString(5, cliente.getSexo());
+			statement.setString(5, cliente.getSexo());
 			statement.setString(6, cliente.getNacionalidad());
-			statement.setString(7, cliente.getDireccion());
-			statement.setString(8, cliente.getLocalidad());
-			statement.setString(9, cliente.getProvincia());
-			statement.setString(10, cliente.getEmail());
-			statement.setString(11, cliente.getTelefono());
-			
-			statement.setString(12, cliente.getUsuario());
-			statement.setString(13, cliente.getContraseña());
-			statement.setBoolean(14, cliente.getAdministrador());
-			statement.setBoolean(15, cliente.getEstado());
-			statement.setString(16, cliente.getTelefono_fijo());*/
+			statement.setDate(7,(Date) cliente.getNacimiento());
+			statement.setString(8, cliente.getDireccion());
+			statement.setString(9, cliente.getLocalidad());
+			statement.setString(10, cliente.getProvincia());
+			statement.setString(11, cliente.getEmail());
+			statement.setString(12, cliente.getTelefono());
+			statement.setString(13, cliente.getUsuario());
+			statement.setString(14, cliente.getContraseña());
+			statement.setBoolean(15, cliente.getAdministrador());
+			statement.setBoolean(16, cliente.getEstado());
+			statement.setString(17, cliente.getTelefono_fijo());
 			
 			if(statement.executeUpdate() > 0){
 				conexion.commit();
--- src/Presentacion/Controlador/servletCliente.java
+++ src/Presentacion/Controlador/servletCliente.java
@@ -1,7 +1,11 @@
 package Presentacion.Controlador;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+///import javax.servlet.RequestDispatcher;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,13 +45,25 @@ public class servletCliente extends HttpServlet {
 			
 			Cliente cliente = new Cliente();
 			
+			
+			try {
+				SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+				Date fecha;
+				fecha = (Date) formato.parse(request.getParameter("txtFechaNacimiento"));
+				cliente.setNacimiento(fecha);
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 			cliente.setDni(request.getParameter("txtDni"));
 			cliente.setCuil(request.getParameter("txtCuil"));
 			cliente.setNombre(request.getParameter("txtNombre"));
 			cliente.setApellido(request.getParameter("txtApellido"));
-			cliente.setSexo(request.getParameter("ddlSexo"));
+			cliente.setSexo(request.getParameter("ddlSexo").toString());
 			cliente.setNacionalidad(request.getParameter("txtNacionalidad"));
-		
 			cliente.setDireccion(request.getParameter("txtDireccion"));
 			cliente.setLocalidad(request.getParameter("txtLocalidad"));
 			cliente.setProvincia(request.getParameter("txtProvincia"));
@@ -55,11 +71,10 @@ public class servletCliente extends HttpServlet {
 			cliente.setTelefono_fijo(request.getParameter("txtTelFijo"));
 			cliente.setTelefono(request.getParameter("txtTelCel"));
 			cliente.setUsuario(request.getParameter("txtUsuarioCliente"));
-			//cliente.setContraseña(request.getParameter("txtContraseña"));
-			
+			cliente.setContraseña(request.getParameter("txtContraseña"));
+			cliente.setAdministrador(true);
+			cliente.setEstado(true);
 			clienteNeg.insertar(cliente);
-			
-			
 			
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/RegistroClienteAdmin.jsp");
 			dispatcher.forward(request, response);
